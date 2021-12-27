5da9c2441b483ec51e87913adc0fb41b4b5d0446
加藤秀征 <xxwxwxx1@gmail.com>
2021-12-27 17:40:14 +0900

    Callableインターフェースの使用
    callメソッド

---
+++ Study/03/chapter_10/SampleCallable.java
@@ -0,0 +1,59 @@
+package chapter_10;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+/**
+ * Callableインターフェースの使用例<br>
+ * - callメソッドを一つだけ持つ<br>
+ *
+ */
+public class SampleCallable{
+  public static void main(String[] a) throws InterruptedException, ExecutionException {
+    executeCallable();
+  }
+
+  /**
+   * Callableのインスタンスを生成<br>
+   * - 偶数判定ができるよう、callメソッドをオーバーライド<br>
+   * - スレッドの戻り値（結果）をlistに追加<br>
+   * - 偶数のみカウント<br>
+   * @throws ExecutionException
+   * @throws InterruptedException
+   *
+   */
+  private static void executeCallable() throws InterruptedException, ExecutionException {
+    ExecutorService exec = Executors.newSingleThreadExecutor();
+
+    // 偶数判定
+    Callable<Boolean> task = new Callable<Boolean>() {
+      @Override
+      public Boolean call() {
+        return new Random().nextInt() % 2 == 0;
+      }
+    };
+
+    List<Future<Boolean>> futures = new ArrayList<>();
+    for (int i = 0; i < 10; i++) {
+      futures.add(exec.submit(task));
+    }
+
+    int total = 0;
+    for (Future<Boolean> future : futures) {
+      Boolean result = future.get();
+      System.out.println(result);
+
+      if (result) {
+        total++;
+      }
+    }
+    System.out.println(total);
+  }
+
+}
---
+++ Study/03/chapter_10/SampleCallableLambda.java
@@ -0,0 +1,54 @@
+package chapter_10;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+/**
+ * Callableインターフェースの使用例<br>
+ * - ラムダver<br>
+ *
+ */
+public class SampleCallableLambda {
+  public static void main(String[] a) throws InterruptedException, ExecutionException {
+    executeCallable();
+  }
+
+  /**
+   * Callableのインスタンスを生成<br>
+   * - ラムダで表現した場合、どのインターフェースを使っているのかを以下で判断する<br>
+   * --- 戻り値があれば、Runnableインターフェース<br>
+   * --- 戻り値がなければ、Callableインターフェース<br>
+   *
+   * @throws ExecutionException<br>
+   * @throws InterruptedException<br>
+   */
+  private static void executeCallable() throws InterruptedException, ExecutionException {
+    ExecutorService exec = Executors.newSingleThreadExecutor();
+
+    // callメソッドのオーバーライドをラムダで表現（戻り値があるからCallableインターフェース)
+    List<Future<Boolean>> futures = new ArrayList<>();
+    for (int i = 0; i < 10; i++) {
+      futures.add(exec.submit(() -> {
+        return new Random().nextInt() % 2 == 0; // 戻り値
+      }));
+    }
+
+    int total = 0;
+    for (Future<Boolean> future : futures) {
+      Boolean result = future.get();
+      System.out.println(result);
+
+      if (result) {
+        total++;
+      }
+    }
+    System.out.println(total);
+  }
+
+}
