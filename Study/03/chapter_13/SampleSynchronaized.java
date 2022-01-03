4158f7f71dbaec2d15390d11c3c6cda0c74f27e5
加藤秀征 <xxwxwxx1@gmail.com>
2022-01-03 13:17:32 +0900

    競合しないプログラム

--- Study/03/chapter_13/SampleSynchronaized.java
+++ Study/03/chapter_13/SampleSynchronaized.java
@@ -4,8 +4,10 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 /**
- * スレッド競合した場合<br>
+ * A)スレッド競合した場合<br>
  * - 300を出力されるが、何回かに一回スレッド競合が発生し、200が表示される<br>
+ * B)排他制御をかけた場合<br>
+ * - addメソッド宣言時にsynchronized修飾子を宣言し、排他制御をかける<br>
  *
  */
 public class SampleSynchronaized implements Runnable {
@@ -29,7 +31,12 @@ public class SampleSynchronaized implements Runnable {
   }
 
   // 引数をnumフィールドに加算
-  public void add(int amount) {
+//  public void add(int amount) { // A
+//    this.num += amount;
+//  }
+
+  //
+  public synchronized void add(int amount) { // B
     this.num += amount;
   }
 
