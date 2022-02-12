85e0b237b6a476abdf9b9db8a7c7fe6a1c3baf74
加藤秀征 <xxwxwxx1@gmail.com>
2022-02-12 16:09:24 +0900

    JDBC/データベース executeBatchメソッド

---
+++ Study/06/chapter_13/SampleBatch.java
@@ -0,0 +1,57 @@
+package chapter_13;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.function.BiConsumer;
+import java.util.function.Supplier;
+
+import DBInformation.DB;
+
+/**
+ * JDBC/データベース<br>
+ * executeBatchメソッド<br>
+ */
+public class SampleBatch {
+  private static String sql = "update ITEM set price = ? where name = ?;";
+  private static int[] results; // SQLの実行結果
+  private static Supplier<String> creRandom = () -> String.valueOf(new Random().nextInt(500)); // 乱数を文字列で返す
+
+  // sql文に値をセットするBiConsumer型ラムダ
+  // 内部で自作のBiConsumer型acceptTryメソッドを呼び出し、呼び出し先で生のBiConsumer.acceptメソッドを実行
+  private static BiConsumer<PreparedStatement, String> setParameter = ThrowableBiConsumer.acceptTry((ps, name) -> {
+    ps.setString(1, creRandom.get());
+    ps.setString(2, name);
+    ps.addBatch();
+  });
+
+  public static void main(String[] args) {
+    batchExe();
+  }
+
+  /**
+   * ITEMリストからストリームを生成し、priceカラムにランダムな値をセットし、DBを更新する<br>
+   * executeBatchメソッド<br>
+   * - 一度に複数のSQL文を実行できる<br>
+   * - 戻り値は各SQL文の更新件数の配列であり、すべての更新件数が合算される訳ではない<br>
+   */
+  private static void batchExe() {
+    List<String> itemList = Arrays.asList("apple", "banana", "orange");
+    try (Connection con = DriverManager.getConnection(DB.URL, DB.USER, DB.PASS)) {
+      try (var ps = con.prepareStatement(sql)) {
+        itemList.stream().forEach(name -> {
+          setParameter
+          .accept(ps, name);
+        });
+        results = ps.executeBatch();
+        System.out.println(Arrays.toString(results));
+      }
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+  }
+}
---
+++ Study/06/chapter_13/ThrowableBiConsumer.java
@@ -0,0 +1,37 @@
+package chapter_13;
+
+import java.sql.SQLException;
+import java.util.function.BiConsumer;
+
+/**
+ * 呼び出し元のラムダでtry文を書きたくなかったから、Exception対応のBiConsumer型インターフェースを生成<br>
+ * - インターフェースを作るハメになり、コードが見やすくなったかどうかは不明<br>
+ */
+public interface ThrowableBiConsumer<T, U> {
+  void accept(T t, U u) throws SQLException;
+
+  /**
+   * 処理内容はBiConsumer.acceptメソッドと全く同じ<br>
+   * - BiConsumer型のインスタンスを受け取る<br>
+   * - 内部で受け取ったBiConsumerインスタンスで、生のacceptメソッドを実行し、その結果を<br>
+   * - 例外が起きた場合、acceptTryメソッド内でcatchするため、呼び出し元で例外処理を記述しない<br>
+   *
+   * @param biCon<PreparedStatement, String> <br>
+   * @return
+   */
+  public static /* 総称型 */ <T, U> BiConsumer<T, U> acceptTry(ThrowableBiConsumer<T, U> biCon) {
+    return (t, u) -> {
+      try {
+        biCon.accept(t, u);
+      } catch (SQLException e) {
+        e.printStackTrace();
+        System.out.println("acceptメソッドで例外をキャッチしました。");
+      }
+    };
+  }
+}
+/**
+ * 総称型のメモ<br>
+ * 戻り値の前に<>ジェネリクスを定義する<br>
+ * 呼び出す際にジェネリクスを指定することで、任意の型でacceptTryメソッドを実行することができる<br>
+ */
