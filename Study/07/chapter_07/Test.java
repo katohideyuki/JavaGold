06f643a416d1868058ced9847c45e74b1bdb0501
加藤秀征 <xxwxwxx1@gmail.com>
2022-02-13 23:39:57 +0900

    汎用とコレクション 非境界ワイルドカード「?」が使い勝手の良いモノではなかった

---
+++ Study/07/chapter_07/Test.java
@@ -0,0 +1,23 @@
+package chapter_07;
+
+/**
+ * どんな型でも扱えるクラス<br>
+ */
+public class Test<T> {
+  T value;
+
+  public Test(T value) {
+    super();
+    this.value = value;
+  }
+
+  public T getValue() {
+    return value;
+  }
+
+  public void setValue(T value) {
+    this.value = value;
+  }
+
+
+}
