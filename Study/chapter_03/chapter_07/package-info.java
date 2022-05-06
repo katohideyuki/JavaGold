741766b2a3dd8e763c3cfac3617d74506051d9ac
加藤秀征 <xxwxwxx1@gmail.com>
2022-05-06 11:39:27 +0900

    Executors.newScheduleThreadPool メソッドの使用例

--- Study/chapter_03/chapter_07/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Executorsクラス<br>
- * - newScheduledThreadPoolメソッド<br>
- * --- あらかじめ複数のスレッドを生成して保持する<br>
- */
-package chapter_07;
---
+++ Study/chapter_03/sec07_SchedulePool/UseScheduleThreadPool.java
@@ -0,0 +1,35 @@
+package sec07_SchedulePool;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import ch02_commonUtil.Sleep;
+
+/** Executors.newScheduleThreadPool メソッドの使用例 */
+public class UseScheduleThreadPool {
+    public static void main(String[] args) {
+        performTwoTasks();
+    }
+
+    /* 2つのスレッドを生成して、2つのタスクを実行 */
+    private static void performTwoTasks() {
+        ScheduledExecutorService exec = Executors.newScheduledThreadPool(2);
+
+        // 1つ目のタスク
+        exec.scheduleWithFixedDelay(() -> {
+            System.out.println("First task  : A");
+        }, 1, 1, TimeUnit.SECONDS);
+
+        // 2つ目のタスク
+        exec.scheduleWithFixedDelay(() -> {
+            System.out.println("second task : B");
+        }, 1, 1, TimeUnit.SECONDS);
+
+        // 10秒後にスレッドプールを閉じる
+        Sleep.exe(10000);
+        exec.shutdown();
+        if (exec.isShutdown())
+            System.out.println("finish.");
+    }
+}
