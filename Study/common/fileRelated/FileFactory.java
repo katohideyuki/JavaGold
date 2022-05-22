04465d304858576f82f1667b6c5d9a1d425fc026
加藤秀征 <xxwxwxx1@gmail.com>
2022-05-22 21:22:57 +0900

    BufferedWriter インスタンス生成メソッド追加

--- Study/common/fileRelated/FileFactory.java
+++ Study/common/fileRelated/FileFactory.java
@@ -1,16 +1,21 @@
 package fileRelated;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 /** 例外処理を記述すると冗長になるため File 型に関するインスタンス生成などを切り離す
       - 現時点で何が汎用性高いのか判断つかないため、必要なときに必要な処理を追加していき
         終盤でリファクタリングを行う */
 public class FileFactory {
-    private static final String DEFAULT_PATH = "src/main/resource/chapter_05/default.txt";
+    // 読み込み用デフォルトファイル
+    static final String DEFAULT_READ_PATH  = "src/main/resource/chapter_05/default.txt";
+    // 書き込み用デフォルトファイル
+    static final String DEFAULT_WRITE_PATH = "src/main/resource/chapter_05/default_write.txt";
 
     /* FileReader インスタンスを返却
          - ファイルが存在しなければデフォルトのFileReader インスタンスを返却 */
@@ -18,23 +23,40 @@ public class FileFactory {
         try {
             if (file.exists())
                 return new FileReader(file);
-            return new FileReader(new File(DEFAULT_PATH));
+            return new FileReader(new File(DEFAULT_READ_PATH));
         } catch (FileNotFoundException e) {
-            System.out.printf("file not found : %s%n", e);
+            System.out.printf("file not found : %s%n", file);
             throw new RuntimeException(e);
         }
     }
 
-    /* BufferdReader インスタンスを返却
-         - ファイルが存在しなければデフォルトのBufferedReader インスタンスを返却 */
+    /* BufferedReader インスタンスを返却
+         - ファイルが存在しなければデフォルトの BufferedReader インスタンスを返却 */
     public static BufferedReader creBufferedReader(String path) {
         File file = new File(path);
         try {
             if (file.exists())
                 return new BufferedReader(new FileReader(file));
-            return new BufferedReader(new FileReader(new File(path)));
+            return new BufferedReader(new FileReader(new File(DEFAULT_READ_PATH)));
         } catch (IOException e) {
-            System.out.printf("file not found : %s%n", e);
+            System.out.printf("file not found : %s%n", path);
+            throw new RuntimeException(e);
+        }
+    }
+
+    /* BufferedWriter インスタンスを返却
+         - 同名のディレクトリが存在していれば、デフォルトの BufferedWriter インスタンスを返却 */
+    public static BufferedWriter creBufferedWriter(String path) {
+        File file = new File(path);
+        try {
+            if (!file.isDirectory()) {
+                System.out.printf("written to file : %s%n", file);
+                return new BufferedWriter(new FileWriter(file, true));
+            }
+            System.out.printf("written to default file : %s%n", DEFAULT_WRITE_PATH);
+            return new BufferedWriter(new FileWriter(DEFAULT_WRITE_PATH, true));
+        } catch (IOException e) {
+            System.out.printf("same name already exists : %s%n", path);
             throw new RuntimeException(e);
         }
     }
