ba307985f21189c2d5e2d2d2bc075ae3c1765545
加藤秀征 <xxwxwxx1@gmail.com>
2022-05-29 17:56:07 +0900

    PreparedStatement.setXXX の使用例

--- Study/chapter_06/sec07_PreparedStatement/UseSetXXX.java
+++ Study/chapter_06/sec07_PreparedStatement/UseSetXXX.java
@@ -9,21 +9,35 @@ import debug.Debug;
 
 /** PreparedStatement.setXXX の使用例 */
 public class UseSetXXX {
+    static final String SQL_STR = "insert into item values(?,?,?)";  // (?,?,?) => (1番目の?, 2番目の?. 3番目の?)
+
     public static void main(String[] args) throws SQLException {
         setParameter();
+        noSetParameter();
     }
 
     /* Insert 文にセットするパラメータをメソッドで設定する */
     private static void setParameter() throws SQLException {
         Debug.log("setParameter");
-
-        var sql              = "insert into item values(?,?)";  // (?,?) => (1番目の?, 2番目の?)
         Connection con       = DB.getCon();
-        PreparedStatement ps = con.prepareStatement(sql);
+        PreparedStatement ps = con.prepareStatement(SQL_STR);
         try (con; ps) {
-            ps.setInt(1, 2);            // 1番目の「?」に代入したい値
+            ps.setInt(1, 4);            // 1番目の「?」に代入したい値
             ps.setString(2, "sample");  // 2番目に「?」に代入したい値
-            System.out.println(ps);
+            ps.setInt(3, 400);          // 3番目の「?」に代入したい値
+            ps.executeUpdate();
+        }
+    }
+
+    /* Insert 文にセットするパラメータを未設定にする
+         - 2〜3番目が未設定により SQLException 発生*/
+    private static void noSetParameter() throws SQLException {
+        Debug.log("noSetParameter");
+        Connection con = DB.getCon();
+        PreparedStatement ps = con.prepareStatement(SQL_STR);
+        try (con; ps) {
+            ps.setInt(1, 2);    // 1番目の「?」に代入したい値
+            ps.executeUpdate();
         }
     }
 }
@@ -32,6 +46,8 @@ public class UseSetXXX {
  * <pre>
  * preparedStatement.setXXX(何番目, セットしたい値)
  *   - SQL 文にパラメータを渡したい場合、置き換え文字として「?」を使う
+ *     - パラメータはすべてセットしなければならない
+ *     - 未設定のパラメータがある場合、実行時に SQLException がスローされる
  *   - セットしたい列の型によって、setString, setInt といった setXXX という形式
  *     のメソッドを使う
  *   - 第一引数に渡すのは1から始まる番号
