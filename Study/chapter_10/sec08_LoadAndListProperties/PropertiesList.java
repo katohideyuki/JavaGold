0dcbb71fc1d469e9bae639edf7713761e5b65a4e
加藤秀征 <xxwxwxx1@gmail.com>
2022-06-19 20:32:23 +0900

    Properties の使用例

---
+++ Study/chapter_10/sec08_LoadAndListProperties/PropertiesList.java
@@ -0,0 +1,34 @@
+package sec08_LoadAndListProperties;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
+import debug.Debug;
+
+/** Properties の使用例 */
+public class PropertiesList {
+    public static void main(String[] a) {
+        exe();
+    }
+
+    /* プロパティファイルの値を一覧表示 */
+    private static void exe() {
+        Debug.log("exe");
+        Properties prop = new Properties();
+        try {
+            prop.load(new FileReader("db_setting.properties"));
+            prop.list(System.out); // PrintStreamクラスのoutフィールドの参照を渡す
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+
+/**
+ * <pre>
+ * Properties.list(PrintStream 型 または printWriter 型)
+ *   - プロパティファイルの中身を一覧表示する
+ *   - デバッグ時などで使用
+ * </pre>
+ */
