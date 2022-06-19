15c04266ec05612bd7ec545778c802f792401be0
加藤秀征 <xxwxwxx1@gmail.com>
2022-06-19 13:50:01 +0900

    Locale の使用例 - コンストラクタを使ってインスタンスを生成

---
+++ Study/chapter_10/sec02_LocaleInstance/SampleLocale.java
@@ -0,0 +1,37 @@
+package sec02_LocaleInstance;
+
+import java.util.Locale;
+
+import debug.Debug;
+
+/** Locale の使用例 - コンストラクタを使ってインスタンスを生成 */
+public class SampleLocale {
+    public static void main(String[] args) {
+        creLocaleInstance();
+    }
+
+    private static void creLocaleInstance() {
+        Debug.log("creLocaleInstance");
+
+        // コンストラクタには3パターンある
+        Locale firstLocale  = new Locale("ja");                 // 引数に「言語」
+        Locale secondLocale = new Locale("ja", "JP");           // 引数に「言語、国」
+        Locale thirdLocale  = new Locale("ja", "JP", "MAC");    // 引数に 「言語、国、派生情報」※1
+
+        System.out.println(firstLocale);
+        System.out.println(secondLocale);
+        System.out.println(thirdLocale);
+    }
+}
+
+/**
+ * <pre>
+ * Loale のインスタンス生成方法は5つある
+ *   1. Locale.getDefault()
+ *   2. コンストラクタ
+ *     - ※1 派生情報は、ベンダーまたはブラウザに固有するコード
+ *   3. ロケール定数
+ *   4. ビルダー
+ *   5. ファクトリメソッド
+ * </pre>
+ */
