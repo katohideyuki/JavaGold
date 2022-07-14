e50ff040d9b2c8295685f097acb74966f2c81fa8
加藤秀征 <xxwxwxx1@gmail.com>
2022-07-14 23:51:47 +0900

    とある期間の日付ストリームを返却する

---
+++ Study/SideStory/dayStream/DaysStream.java
@@ -0,0 +1,47 @@
+package dayStream;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.stream.Stream;
+
+import debug.Debug;
+
+/** DateTimeAPI で日付のストリームを生成する */
+public class DaysStream {
+    public static void main(String[] args) {
+        useCreBetween();
+    }
+
+
+    /**
+     * fromDate ~ toDate までの日付を出力する
+     */
+    private static void useCreBetween() {
+        Debug.log("useCreBetween");
+        var pattern = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+        var fromDate = LocalDate.parse("2022/07/01", pattern);
+        var toDate   = LocalDate.parse("2022/07/31", pattern);
+        creBetween(fromDate, toDate).forEach(System.out::println);
+    }
+
+    /**
+     * <pre>
+     * 開始日 〜 終了日 の期間の日付をストリームで返却する
+     * @param fromDate 開始日
+     * @param toDate   終了日
+     * @return 期間
+     * </pre>
+     */
+    private static Stream<LocalDate> creBetween(LocalDate fromDate, LocalDate toDate) {
+        Debug.log(null);
+        return Stream.iterate(fromDate, d -> d.plusDays(1)).takeWhile(d -> !d.isAfter(toDate));
+    }
+}
+/**
+ * Stream.takeWhile(Predicate 型)
+ *   - 条件を満たさなくなったら、処理を終了する。
+ *
+ * Locale.isAfter(LocalDate 型)
+ *   - 指定された日付より未来であれば true を返却する。
+ *
+ */
